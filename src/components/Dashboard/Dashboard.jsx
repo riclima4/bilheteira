e4bbc20c007dc08fe89e13d5bfe60e54a38bd036
Ricardo Lima <ric.lima1@hotmail.com>
2022-12-03 18:24:00 +0000

    tickets done

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -9,10 +9,37 @@ export default function Dashboard() {
       <h3 className="descText">Comprem bilhetes para os eventos disponíveis</h3>
       <div className="tickets flex">
         <div className="leftSide">
-          <h2>Titulo do Evento</h2>
-          <p>Descrição do evento</p>
+          <div className="flex titleAndDate">
+            <h2>Titulo do Evento</h2>
+            <h3>18/12/2022</h3>
+          </div>
+          <p>Local: Coliseu do Porto</p>
+          <p>
+            Descrição do evento Concerto Led zeppling onde te vais divertir ao
+            maximo
+          </p>
         </div>
-        <div className="rightSide">Horario e INFO</div>
+        <div className="rightSide flex">
+          <p>35.00€</p>
+          <button>Comprar</button>
+        </div>
+      </div>
+      <div className="tickets flex">
+        <div className="leftSide">
+          <div className="flex titleAndDate">
+            <h2>Titulo do Evento</h2>
+            <h3>18/12/2022</h3>
+          </div>
+          <p>Local: Coliseu do Porto</p>
+          <p>
+            Descrição do evento Concerto Led zeppling onde te vais divertir ao
+            maximo
+          </p>
+        </div>
+        <div className="rightSide flex">
+          <p>35.00€</p>
+          <button>Comprar</button>
+        </div>
       </div>
     </div>
   );
