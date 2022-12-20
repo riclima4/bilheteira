a37a31e7bd758683a0da71118a9d99f27ddf8a0e
Ricardo Lima <ric.lima1@hotmail.com>
2022-12-20 20:32:27 +0000

    footer

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,28 +2,32 @@ import React from "react";
 import "./dashboard.css";
 import Event from "../../components/Evento/Event";
 import { Grid } from "@mui/material";
+import Footer from "../../components/Footer/Footer";
 
 export default function Dashboard() {
   return (
-    <div className="DashboardContent">
-      <div className="banner">
-        <h1 className="tituloText">Bem-vindos à PurpleTicket</h1>
-        <h3 className="descText">
-          Comprem bilhetes para os eventos disponíveis
-        </h3>
-      </div>
+    <div className="page-container">
+      <div className="DashboardContent">
+        <div className="banner">
+          <h1 className="tituloText">Bem-vindos à PurpleTicket</h1>
+          <h3 className="descText">
+            Comprem bilhetes para os eventos disponíveis
+          </h3>
+        </div>
 
-      <div className="eventos">
-        <Grid
-          container
-          rowSpacing={2}
-          columnSpacing={2}
-          sx={{
-            justifyContent: "center",
-          }}
-        >
-          <Event />
-        </Grid>
+        <div className="eventos">
+          <Grid
+            container
+            rowSpacing={2}
+            columnSpacing={2}
+            sx={{
+              justifyContent: "center",
+            }}
+          >
+            <Event />
+          </Grid>
+        </div>
+        <Footer />
       </div>
     </div>
   );
