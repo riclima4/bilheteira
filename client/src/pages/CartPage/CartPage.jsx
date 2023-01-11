7c95dc2d0ff36975d99d1fff09c98ecc06eb50f9
Ricardo Lima <ric.lima1@hotmail.com>
2023-01-11 20:49:17 +0000

    Update CartPage.jsx

--- client/src/pages/CartPage/CartPage.jsx
+++ client/src/pages/CartPage/CartPage.jsx
@@ -75,11 +75,14 @@ export default function CartPage() {
   const deleteCart = async (idCart) => {
     const res = await axios.delete(cartUrlDelete + idCart);
     if (res) {
-      setOpenToast1(true);
+      setOpen(true);
+      setTimeout(() => {
+        setOpen(false);
+        setOpenToast1(true);
+      }, 2000);
       setTimeout(() => {
         window.location.reload(false);
-      }, 2000);
-      setOpen(true);
+      }, 2500);
     }
     return "not done";
   };
@@ -178,13 +181,13 @@ export default function CartPage() {
           </div>
         </div>
       </div>
-      <Snackbar open={openToast1} autoHideDuration={2000} onClose={handleClose}>
+      <Snackbar open={openToast1} autoHideDuration={1000} onClose={handleClose}>
         <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
           Bilhete Removido com Sucesso!
         </Alert>
       </Snackbar>
 
-      <Snackbar open={openToast2} autoHideDuration={2000} onClose={handleClose}>
+      <Snackbar open={openToast2} autoHideDuration={1000} onClose={handleClose}>
         <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
           Compra efetuada com Sucesso!
         </Alert>
