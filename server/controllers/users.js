54a876f5799d9052127990f29331c9555e94dd99
Ricardo Lima <ric.lima1@hotmail.com>
2023-01-11 22:14:34 +0000

    DONE!!!!!!!!

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,7 @@
 import { createToken } from "../utils/jwt.js";
 import { UserModel } from "../models/users.js";
+import { HistoryModule } from "../models/history.js";
+import { CartModule } from "../models/cart.js";
 
 export const getALLUsers = async (req, res) => {
   const users = await UserModel.findAll();
@@ -37,6 +39,19 @@ export const newUser = async (req, res) => {
 
   res.send(token);
 };
+export const newUserPage = async (req, res) => {
+  const newUser = {
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+  };
+  const asd = await UserModel.create(newUser);
+  const { password, ...user } = asd.dataValues;
+
+  const token = createToken(user);
+
+  res.redirect("http://localhost:5500/account/");
+};
 
 export const updateUser = async (req, res) => {
   const id = req.params.id;
@@ -56,14 +71,31 @@ export const updateUser = async (req, res) => {
 };
 
 export const deleteUsers = async (req, res) => {
-  const id = req.params.id;
-  const user = await UserModel.findByPk(id);
-  if (user !== null) {
-    user.destroy({ where: { id: id } });
-    return res.send("User  Deleted");
-  } else {
-    return res.send("Não existe User com id: " + id);
+  const idUser = req.params.id;
+  const user = await UserModel.findByPk(idUser);
+  const cart = await CartModule.findAll({ where: { idUser: idUser } });
+  const userHistory = await HistoryModule.findAll({
+    where: { idUser: idUser },
+  });
+  if (cart !== null) {
+    cart.forEach((item) => {
+      item.destroy({ where: { idUser: idUser } });
+    });
   }
+  if (userHistory !== null) {
+    userHistory.forEach((item) => {
+      item.destroy({ where: { idUser: idUser } });
+    });
+  }
+
+  setTimeout(() => {
+    if (user !== null) {
+      user.destroy({ where: { idUser: idUser } });
+      res.send("Funfa");
+    } else {
+      res.send("Não existe User com id: " + idUser);
+    }
+  }, 2000);
 };
 
 export const login = async (req, res) => {
